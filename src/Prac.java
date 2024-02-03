68ae411c131a82eeced68c7e8450151e410734ae
RAGAVI2904 <ragavia1999@gmail.com>
2024-02-03 14:36:02 +0530

    commit2

--- src/Prac.java
+++ src/Prac.java
@@ -1,11 +1,14 @@
 public class Prac {
     public static void m1(){
-        System.out.println("");
+        System.out.println("Hello");
     }
+    public static void m2(){System.out.println("Hii");}
 
     public static void main (String[] args){
         m1();
+        m2();
     }
+
 
 
 }
